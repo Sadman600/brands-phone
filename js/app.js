b9bbf14bd21a15ce19290e0eaaab1039689f74ad
Sadman600 <sadmansakib600@gmail.com>
2022-03-01 20:19:56 +0600

    All Work is done

--- js/app.js
+++ js/app.js
@@ -8,11 +8,13 @@ const singleDisplay = displayStyle => {
     document.getElementById('display-single-phone').style.display = displayStyle;
 }
 const loadPhoneData = () => {
+    alert('none');
+    document.getElementById('display-all-phone').innerHTML = ``;
     const searchInput = document.getElementById('search-input');
-    const searchValue = searchInput.value;
+    const searchValue = searchInput.value.toLowerCase();
     singleDisplay('none');
     spinner('block');
-    document.getElementById('display-all-phone').innerHTML = ``;
+
     // Load Phone Data 
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
@@ -21,9 +23,10 @@ const loadPhoneData = () => {
 
             if (data.data.length == 0) {
                 alert('block');
+                spinner('none');
             } else {
                 alert('none');
-                getPhoneData(data.data);
+                getPhoneData(data.data.slice(0, 20));
             }
         });
     // Clear input search data 
@@ -33,7 +36,7 @@ const loadPhoneData = () => {
 const getPhoneData = phones => {
     const displayAllPhone = document.getElementById('display-all-phone');
     for (const phone of phones) {
-        // console.log(phone);
+
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -59,7 +62,7 @@ const loadSingleData = phoneId => {
 }
 
 const getSingleData = phoneData => {
-    console.log(phoneData);
+
     const sensor = phoneData.mainFeatures.sensors;
     const other = phoneData.others;
     const displaySinglePhone = document.getElementById('display-single-phone');
@@ -73,7 +76,7 @@ const getSingleData = phoneData => {
                 <div class="col-md-8">
                     <div class="card-body">
                         <h5 class="card-title">Name: ${phoneData.name}</h5>
-                        <p class="card-text">Release Date: ${phoneData.releaseDate}</p>
+                        <p class="card-text">Release Date: ${phoneData.releaseDate ? phoneData.releaseDate : ' No release date '}</p>
                         <button onclick='' type="button" class="btn btn-primary" data-bs-toggle="collapse" data-bs-target="#demo">Others</button>
                         <div id="demo" class="collapse">
                             <p><strong>Chip Set:</strong> ${phoneData.mainFeatures.chipSet}</p>
@@ -82,23 +85,23 @@ const getSingleData = phoneData => {
                             <p><strong>Storage:</strong> ${phoneData.mainFeatures.storage}</p>
                             <p><strong>Sensors:</strong>
                                 <ul>
-                                    <li>${sensor[0]}</li>
-                                    <li>${sensor[1]}</li>
-                                    <li>${sensor[2]}</li>
-                                    <li>${sensor[3]}</li>
-                                    <li>${sensor[4]}</li>
-                                    <li>${sensor[5]}</li>
-                                    <li>${sensor[6]}</li>
+                                    <li>${sensor[0] ? sensor[0] : 'No available'}</li>
+                                    <li>${sensor[1] ? sensor[1] : 'No available'}</li>
+                                    <li>${sensor[2] ? sensor[2] : 'No available'}</li>
+                                    <li>${sensor[3] ? sensor[3] : 'No available'}</li>
+                                    <li>${sensor[4] ? sensor[4] : 'No available'}</li>
+                                    <li>${sensor[5] ? sensor[5] : 'No available'}</li>
+                                    <li>${sensor[6] ? sensor[6] : 'No available'}</li>
                                 </ul>
                             </p>
                             <p><strong>Others:</strong> 
                                 <ul>
-                                    <li><strong>WLAN:</strong> ${other.WLAN}</li>
-                                    <li><strong>Bluetooth:</strong> ${other.Bluetooth}</li>
-                                    <li><strong>GPS:</strong> ${other.GPS}</li>
-                                    <li><strong>USB:</strong> ${other.USB}</li>
-                                    <li><strong>Radio:</strong> ${other.Radio}</li>
-                                    <li><strong>NFC:</strong> ${other.NFC}</li>
+                                    <li><strong>WLAN:</strong> ${other?.WLAN ? other?.WLAN : 'No available'}</li>
+                                    <li><strong>Bluetooth:</strong> ${other?.Bluetooth ? other?.Bluetooth : 'No available'}</li>
+                                    <li><strong>GPS:</strong> ${other?.GPS ? other?.GPS : 'No available'}</li>
+                                    <li><strong>USB:</strong> ${other?.USB ? other?.USB : 'No available'}</li>
+                                    <li><strong>Radio:</strong> ${other?.Radio ? other?.Radio : 'No available'}</li>
+                                    <li><strong>NFC:</strong> ${other?.NFC ? other?.NFC : 'No available'}</li>
                                     
                                 </ul>
                             </p>
